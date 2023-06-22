00ead62d34e2508b82d1c06c4e85379a1d0ef0cb
chato1337 <chato1337@gmail.com>
2023-06-21 21:13:27 -0500

    implements env variables

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,9 +1,9 @@
-
+require('dotenv').config();
 
 export const config = {
-    dbName: 'logistics',
-    dbUser: 'root',
-    dbHost: 'localhost',
-    dbPassword: '1814',
-    port: 3306,
+    dbName: process.env.DATABASE_NAME || 'env_error',
+    dbUser: process.env.DATABASE_USER || 'env_error',
+    dbHost: process.env.DATABASE_HOST || 'env_error',
+    dbPassword: process.env.DATABASE_PASS || 'env_error',
+    port: process.env.APP_PORT || 'env_error',
 }
